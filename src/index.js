48acc7201b7788d80330ff5554511bb4e9c8ad38
fabio <fabgmad@gmail.com>
2019-12-16 22:56:59 -0300

    Added Redux Persist to the Application and applied Local Storage

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 
 // Components Dependencies
@@ -10,12 +11,15 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={ store } >
+  <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+       <App />
+      </PersistGate>
     </BrowserRouter> 
   </Provider>, 
   document.getElementById('root')
   );
 
 
+  // PersistGate is the last step to implement the Local Storage persistence in our application. With the PersistGate, we can: (i) receive the Store; (ii) persist the Store, in order to "rehydrate" it with the Root Reducer current state. So, whenever there is a refresh, or the closing of a tab or the browser, the reducer state will initialize as empty, and then there will be the rehydration of the reducer, bringing what is in the cache, updating the reducer state to what it was in the current state.
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
@@ -6,6 +7,10 @@ import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+// The persistor is using the redux-persist library, so we can make the configurations necessary to create a 'cache' of the Store, which will allow the application to persist the store, and therefore, to persist the currentState, even if there is a refresh, or if the user closes the tab/browser.
+
+export const persistor = persistStore(store);
+
+export default { store, persistor };
